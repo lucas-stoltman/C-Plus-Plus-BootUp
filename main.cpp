9c325b47daa17d13f2b8aa848ef0080dbd38f48c
UWB CSS <uwbcssbootup@csslab8h.uwb.edu>
2021-09-16 20:12:31 -0700

    user input example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 // Created by Lucas Stoltman 2021
 
+// HELLO WORLD EXAMPLE
+/*
 #include <iostream>
 
 int main() {
     std::cout << "Hello World" << std::endl;  
+}
+*/
+
+BASIC USER INPUT EXAMPLE
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "How old are you (in years)?" << endl;
+    cin >> age;
+    cout << "that is " << (365*age) << " days." << endl;
 }
